bee45d52dbb0f16765f4dec0a09e604503b9de02
BrianSHa <brianhainbox@gmail.com>
2017-01-13 14:53:04 -0800

    Add Bootstrap and nav bar.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import Header from './Header';
 
 export default class App extends Component {
   render() {
     return (
-      <h1>Hello, world.</h1>
+      <div>
+        <Header/>
+        <h1> test </h1>
+      </div>
     );
   }
 }
