d9e49faaa23a146e4045c2dd50964c22e9ee5ff6
BrianSHa <brianhainbox@gmail.com>
2017-01-16 07:49:25 -0800

    Fixed list rendering.

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,34 +1,38 @@
 import React, { Component } from 'react';
-import ListItem from './ListItem';
+// import ListItem from './ListItem';
 
-export default class List extends Component {
-    constructor() {
-        super();
-        this.state = {
-            value: [
-                {
-                    id: 1,
-                    title: 'test',
-                    body: 'test-paragraph'
-                },
-                {
-                    id: 2,
-                    title: 'test2',
-                    body: 'test-paragraph2'
-                }
-            ]
-        };
+const testList = [
+    {
+        id: 1,
+        title: 'test',
+        body: 'test-paragraph'
+    },
+    {
+        id: 2,
+        title: 'test2',
+        body: 'test-paragraph2'
     }
+];
+
+function MyList(props) {
+    const list = props.value;
+    const listItems = list.map((item, i) =>
+        <li key={i}>{item.id} {item.title} {item.body}</li>
+    );
+    return (
+        <ul>{listItems}</ul>
+    );
+}
+export default class List extends Component {
     render() {
         return (
             <div>
                 <h2>Notifications</h2>
                 <div>
-                    <ul>
-                        <ListItem list={this.state.value}/>
-                    </ul>
+                    <MyList value={testList} />
                 </div>
             </div>
         );
     }
 }
+
