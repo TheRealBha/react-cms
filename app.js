e820fca8582d01b231fce0ff7c29280d4042ad58
BrianSHa <brianhainbox@gmail.com>
2017-01-17 21:40:09 -0800

    Adding swagger-ui to project and static files.

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const path = require('path');
 const webpack = require('webpack');
 const express = require('express');
 const config = require('./webpack.config');
+const openapi = require('express-openapi');
+const v1WorldsService = require('./api-v1/service/test-service');
+const v1ApiDoc = require('./api-v1/api-doc');
 
 const app = express();
 const compiler = webpack(config);
@@ -12,9 +15,35 @@ app.use(require('webpack-dev-middleware')(compiler, {
 
 app.use(require('webpack-hot-middleware')(compiler));
 
-app.get('*', function(req, res) {
+app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
+
+ console.log(v1ApiDoc);
+console.log(v1WorldsService);
+
+openapi.initialize({
+  app,
+  apiDoc: v1ApiDoc,
+  exposeApiDocs: true,
+  dependencies: {
+    worldsService: v1WorldsService
+  },
+  paths: './api-v1/paths',
+  errorMiddleware (err, req, res, next) {
+    console.log(err);
+    res.status(400).json(err);
+  }
+})
+
+app.get('/swagger-ui', (req, res, next)=>{
+  if(!req.query.url){
+    res.redirect('/swagger-ui?url=/v1/apidoc');
+    return;
+  }
+  next();
+})
+app.use('/swagger-ui', express.static('swagger-ui'));
 
 
 app.listen(3000, function(err) {
