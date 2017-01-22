80239f06b192329b46c5f2df0a88ed3fedc3728d
BrianSHa <brianhainbox@gmail.com>
2017-01-21 19:58:40 -0800

    Add insert function.

--- api-v1/service/sqlService.js
+++ api-v1/service/sqlService.js
@@ -1,17 +1,32 @@
 'use strict';
 const config = require('config');
+
+// Conect to DB
 var mysql = require('knex')({
   dialect: 'mysql',
   connection: config.get('db')
 });
- 
+
+// Create CRUD operations
 const sqlService = {
+  // SELECT
   list() {
-    return mysql.select().table('message').then((rows)=>{
+    return mysql.select().table('message').then((rows) => {
       console.log(rows);
       return rows;
     })
+  },
+  //INSERT
+  insert(title, body) {
+    return mysql.insert(title, body).into('message').then((row) => {
+      
+      return {
+        inserted: true,
+        row: row
+      };
+    })
   }
+
 };
- 
+
 module.exports = sqlService;
